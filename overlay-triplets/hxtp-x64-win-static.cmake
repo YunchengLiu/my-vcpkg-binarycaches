6e19cd455861add7b471bbd1931cb914b9fc22e1
YunchengLiu <yuncheng0329@outlook.com>
2025-06-12 10:51:56 +0800

    fix: update destination paths for vcpkg config files in sync.yml

--- overlay-triplets/hxtp-x64-win-static.cmake
+++
@@ -1,8 +0,0 @@
-set(VCPKG_TARGET_ARCHITECTURE x64)
-
-set(VCPKG_CRT_LINKAGE dynamic)
-
-# todo: set same compiler options with main project for all libraries
-# include optimization, debug info, etc.
-
-set(VCPKG_LIBRARY_LINKAGE static)
